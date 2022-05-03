2bf776e297eb2c0458d3c0e136fb9b0210c87d86
jbjzeehad <ilamta.97@gmail.com>
2022-05-03 22:36:21 +0600

    pain

---
+++ CBR#4_A.cpp
@@ -0,0 +1,16 @@
+/*
+
+   @ Jbj_Zeehad
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int w;
+	cin>>w;
+	if(w%2==0 && w>2){
+		cout<<"YES"<<endl;
+	}
+	else
+		cout<<"NO"<<endl;
+}
--- CBR#63_A.cpp
+++ CBR#63_A.cpp
@@ -1,4 +1,8 @@
-<<<<<<< HEAD
+/*
+
+   @ Jbj_Zeehad
+
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -18,29 +22,4 @@ int main(){
 
 			cout<<"NO"<<endl;
 		}
-		
-	
-=======
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-	int n,a,b,c,x=0,y=0,z=0;
-	cin>>n;
-	for(int i=0;i<n;i++){
-		cin>>a>>b>>c;
-		x=x+a;
-		y=y+b;
-		z=z+c;
-	}
-	if(x==0 && y==0 && z==0){
-		cout<<"YES"<<endl;
-	}
-		else{
-
-			cout<<"NO"<<endl;
-		}
-		
-	
->>>>>>> df50eb8454609e7b98e3664a324029a5a34886ff
 }
